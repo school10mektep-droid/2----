5fbb6b523c7301a8e9fbcee1dbe1d400fa5ed1b0
school10mektep-droid <school10mektep@gmail.com>
2026-09-24 16:02:40 +0500

    Обновить main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
 })
